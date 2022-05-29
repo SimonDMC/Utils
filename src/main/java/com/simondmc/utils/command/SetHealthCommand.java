45642d3c3fe15b176277abe7479f0af020249367
SimonDMC <simondmcplayer@gmail.com>
2022-05-29 12:11:33 +0200

    Remove deprecated method call from /sethealth

--- src/main/java/com/simondmc/utils/command/SetHealthCommand.java
+++ src/main/java/com/simondmc/utils/command/SetHealthCommand.java
@@ -5,6 +5,7 @@ import com.simondmc.utils.command.template.SuperCommand;
 import com.simondmc.utils.util.DataType;
 import com.simondmc.utils.util.PlayerUtil;
 import org.bukkit.Sound;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 
 public class SetHealthCommand implements SuperCommand {
@@ -30,8 +31,9 @@ public class SetHealthCommand implements SuperCommand {
         if (args.length > 1) secondPlayer = PlayerUtil.validateSecondPlayer(args[1], p);
         Player target = (secondPlayer == null ? p : secondPlayer);
 
-        if (!DataType.isIntegerWithinBounds(args[0], 1, (int) target.getMaxHealth())) {
-            p.sendMessage("§cEnter a number between 1 and " + Math.round(target.getMaxHealth()));
+        int maxHealth = (int) target.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+        if (!DataType.isIntegerWithinBounds(args[0], 1, maxHealth)) {
+            p.sendMessage("§cEnter a number between 1 and " + Math.round(maxHealth) + ".");
             return;
         }
 
